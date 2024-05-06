5c6ae0b1f1904b2c2d7b3b193852a8bc87432298
Suman Saha <51438020+sahasuman7@users.noreply.github.com>
2024-05-06 14:50:26 +0000

    Var Let Const

---
+++ 01_Basics/var_const_let.js
@@ -0,0 +1,19 @@
+const accounId = 90875632
+let accountEmail = "suman@billionaire.com"
+var accountPass = "selfConfidence"
+accountCity = "Kolkata"
+
+
+// Not Allowed to Change in const 
+// accounId = 123 
+
+
+// Possible to edit let, var variable 
+accountEmail = "suman@g.com"
+
+accountPass = "spirituality"
+
+accountCity = "Bardwan"
+
+console.log(accounId);
+console.table([accountEmail,accountPass,accountCity]);
