93770f1aa379a599b3bb63ed1aa7a44439a6a54c
Suman Saha <51438020+sahasuman7@users.noreply.github.com>
2024-05-09 08:34:21 +0000

    conversion edit added

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,1 +1,60 @@
-let  score = 33
+let  score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+// Typecasting
+let scoreInNumber = Number(score)
+
+console.log(typeof scoreInNumber);
+
+console.log();
+// Some Case 
+
+let id = "22100127AD"
+
+let idToNumber = Number(id)
+
+// It shows the type is number but this is a aplhanumeric string
+console.log(typeof idToNumber);
+
+// printing 
+console.log(idToNumber); // NaN = Not a Number
+// Not a Number is also a 
+let a = NaN
+
+
+// if we try to convert null into number
+let b = null
+
+let B = Number(b)
+console.log(B); // It return 0
+
+
+// if we try to convert undefined into number
+let c = undefined
+let C = Number(c)
+console.log(C) // Return NaN
+
+// For Boolean value
+
+let boolt = true
+let boolf = false
+
+console.log(typeof(boolt),"and", typeof(boolf));
+
+boolf = Number(boolf)
+boolt = Number(boolt)
+
+console.log(typeof(boolt),"and", typeof(boolf));
+
+let isLoggedin = undefined
+let boolLoggedin = Boolean(isLoggedin)
+
+// console.log(boolLoggedin);
+console.log();
+let someNum = true
+let someString = String(someNum)
+someString = someString+"aum"
+console.log(someString,typeof(someString))
