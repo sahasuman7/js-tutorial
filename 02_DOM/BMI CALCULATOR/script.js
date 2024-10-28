5b8d080819c937092ccb55793201e153a1d381d6
Suman Saha <51438020+sahasuman7@users.noreply.github.com>
2024-10-28 18:42:24 +0000

    BMI

--- 02_DOM/BMI CALCULATOR/script.js
+++ 02_DOM/BMI CALCULATOR/script.js
@@ -1,12 +1,25 @@
-function calculateBmi() {
-    let height = document.querySelector('#height').value;
-    let weight = document.querySelector('#weight').value;
-    let resultElement = document.querySelector('#results');
-  
-    height = height / 100;
-  
-    let ans = weight / (height * height);
-  
-    resultElement.innerHTML = ans;
+const formElement = document.querySelector('form');
+
+formElement.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  let height = parseInt(document.querySelector('#height').value);
+  let weight = parseInt(document.querySelector('#weight').value);
+  let result = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    result.innerHTML = `Enter a Valid Height`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    result.innerHTML = `Enter a Valid Weight`;
+  } else {
+    let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    if (bmi < 18.6) {
+      result.innerHTML = `<span>${bmi}</span> <br /><span>Under Weight</span>`;
+    } else if ((bmi > 18.6) & (bmi < 24.9)) {
+      result.innerHTML = `<span>${bmi}</span> <br /><span>Normal Weight</span>`;
+    } else {
+      result.innerHTML = `<span>${bmi}</span> <br /><span>Over Weight</span>`;
+    }
   }
-  
+});
