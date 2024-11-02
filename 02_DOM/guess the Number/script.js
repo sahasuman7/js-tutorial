2911fe2994e2f059e2a444dd61185df3fb688183
Suman Saha <51438020+sahasuman7@users.noreply.github.com>
2024-11-02 18:51:23 +0000

    changes

--- 02_DOM/guess the Number/script.js
+++ 02_DOM/guess the Number/script.js
@@ -1,69 +1,136 @@
-const formElement = document.querySelector('.form');
-let inputElement = document.querySelector('#guessField');
-const output  = document.querySelector('.lowOrHi');
-let guessArray = document.querySelector('.guesses');
-let remainChaince = document.querySelector('.lastResult');
+// const formElement = document.querySelector('.form');
+// let inputElement = document.querySelector('#guessField');
+// const output  = document.querySelector('.lowOrHi');
+// let guessArray = document.querySelector('.guesses');
+// let remainChaince = document.querySelector('.lastResult');
 
-function generateRandom(){
-    return Math.floor(Math.random()*10)+1;
-}
+// function generateRandom(){
+//     return Math.floor(Math.random()*10)+1;
+// }
+
+// let prevGuess = [];
+// let randomNum;
+// let guessCount = prevGuess.length;
+
+
+// formElement.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     if(prevGuess.length == 0){
+//         randomNum = generateRandom();
+//         console.log(randomNum);
+        
+//     }
+//     let value = inputElement.value;
+
+//     if(value == randomNum){
+//         output.innerHTML = 'Success Win The Game';
+//         prevGuess = [];
+//         guessCount  = prevGuess.length;
+//         guessArray.innerHTML = `${prevGuess}`;
+//         remainChaince.innerHTML = `${(10-guessCount)}`;
+
+//     }else{
+//         if(guessCount == 10){
+//             prevGuess = [];
+//             guessCount  = prevGuess.length;
+//             guessArray.innerHTML = `${prevGuess}`;
+//             remainChaince.innerHTML = `${(10-guessCount)}`;
+
+//         }else{
+//             prevGuess.push(value);
+//             guessCount = prevGuess.length;
+//             guessArray.innerHTML = `${prevGuess}`;
+//             remainChaince.innerHTML = `${(10-guessCount)}`;
+//             if(guessCount == 10){
+//                 output.innerHTML = 'Try Again:)'
+//                 prevGuess = [];
+//                 guessCount  = prevGuess.length;
+//                 guessArray.innerHTML = `${prevGuess}`;
+//                 remainChaince.innerHTML = `${(10-guessCount)}`;
+//             }
+//         }
+//     }
+// });
+
+// Another Solution
+
+const randomNumber = Math.floor(Math.random()*10)+1;
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const showResult = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+
+const p = document.createElement('p');
+console.log(p);
 
 let prevGuess = [];
-let randomNum;
-let guessCount = prevGuess.length;
+let numGuess = 1;
+let playGame = true;
 
-
-formElement.addEventListener('submit', function(e){
-    e.preventDefault();
-    if(prevGuess.length == 0){
-        randomNum = generateRandom();
-        console.log(randomNum);
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        validateGuess(guess);
         
-    }
-    let value = inputElement.value;
 
-    if(value == randomNum){
-        output.innerHTML = 'Success Win The Game';
-        prevGuess = [];
-        guessCount  = prevGuess.length;
-        guessArray.innerHTML = `${prevGuess}`;
-        remainChaince.innerHTML = `${(10-guessCount)}`;
+    })
 
-    }else{
-        if(guessCount == 10){
-            prevGuess = [];
-            guessCount  = prevGuess.length;
-            guessArray.innerHTML = `${prevGuess}`;
-            remainChaince.innerHTML = `${(10-guessCount)}`;
+}
 
-        }else{
-            prevGuess.push(value);
-            guessCount = prevGuess.length;
-            guessArray.innerHTML = `${prevGuess}`;
-            remainChaince.innerHTML = `${(10-guessCount)}`;
-            if(guessCount == 10){
-                output.innerHTML = 'Try Again:)'
-                prevGuess = [];
-                guessCount  = prevGuess.length;
-                guessArray.innerHTML = `${prevGuess}`;
-                remainChaince.innerHTML = `${(10-guessCount)}`;
-
-
-
-            }
-
-            
-
-
-
+function validateGuess(guess){
+    if(guess === '' || isNaN(guess) || guess<1 || guess>10){
+        alert("Please Enter a Valid Number")
+    } else{
+        prevGuess.push(guess);
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMessage(`Game Over. Random Number Was ${randomNumber}`);
+            endGame();
+        } else{
+            displayGuess(guess);
+            checkGuess(guess);
         }
-        
+    }
+
+}
+
+function checkGuess(guess){
+    if(guess === randomNumber){
+        displayMessage(`Success!!!`);
+        endGame();
+    }else if(guess< randomNumber){
+        displayMessage(`Number is Lower than Random`);
+    } else{
+        displayMessage(`Number is Higher than Random`);
 
     }
+
+}
+
+function displayGuess(guess){
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}  `;
+    numGuess++;
+    remaining.innerHTML = `${11-numGuess}`
     
-    
+}
 
+function displayMessage(message){
+    showResult.innerHTML = `<h2>${message}</h2>`;
 
+}
 
-});
+function newGame(){
+
+}
+
+function endGame(){
+
+}
+
 
