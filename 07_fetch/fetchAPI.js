daa6dda9e96a5637ce6e9f043647f5e12ca7975a
Suman Saha <51438020+sahasuman7@users.noreply.github.com>
2024-11-11 20:35:49 +0000

    Fetch API

--- 07_fetch/fetchAPI.js
+++ 07_fetch/fetchAPI.js
@@ -1,46 +1,33 @@
-
-
-const promiseThree = new Promise(function(resolve,reject){
-    setTimeout(()=>{
-        let flag= true
-        if(flag){
-            resolve({'username':'Suman Saha','password':'1234'});
-        } else{
-        reject('ERROR!!!')
-        }
-    },2000)
-});
-
-
-async  function resolvePromiseThree(){
-    try{
-        let response = await promiseThree;
-        console.log(response);
-        // console.log(new Date().getSeconds());
-    }
-    catch(error){
-        console.log(error);
-    }
-
-
-}
-// Checking the Waiting Time
-// console.log(new Date().getSeconds());
-resolvePromiseThree()
-
-async function userApiFetch() {
-    try {
-        let response = await fetch('https://jsonplaceholder.typicode.com/users');
-        // console.log(response);
-        // console.log(typeof response);
-        let data = await response.json()
-        console.log(data);
+// async function userApiFetch() {
+//     try {
+//         let response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         // console.log(response);
+//         // console.log(typeof response);
+//         let data = await response.json()
+//         console.log(data);
         
-    } catch (error) {
-        console.log(error);
-    }
+//     } catch (error) {
+//         console.log(error);
+//     }
     
 
-}
+// }
 
-userApiFetch()
+// userApiFetch()
+
+fetch('https://jsonplaceholder.typicode.com/users').then(function(response){
+    return response.json()
+    
+    
+}).then(function(data){
+    console.log(data);
+    
+    
+})
+
+
+
+.catch((error)=>{
+    console.log(error);
+    
+})
